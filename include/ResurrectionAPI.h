ad1c27e2f8bc0a90102824b91b37ee8861da2b22
Daminikov <podymalov@vk.com>
2025-08-19 19:00:54 +0800

    1

--- include/ResurrectionAPI.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class ResurrectionAPI
-{
-public:
-    virtual bool should_resurrect(RE::Actor*) const { return false; };
-    virtual void resurrect(RE::Actor*){};
-};
-typedef void (*AddSubscriber_t)(std::unique_ptr<ResurrectionAPI> api);
